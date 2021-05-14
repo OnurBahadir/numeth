a01e4f542830af83830722dc96807f36664ae6e9
Onur Bahadır <onurbahadirr@gmail.com>
2021-05-14 07:57:31 +0300

    Update equations.h

--- equations.h
+++ equations.h
@@ -5,6 +5,7 @@
 #ifndef NUMETH_EQUATIONS_H
 #define NUMETH_EQUATIONS_H
 namespace equation {
+    
     // f1(x)= x^3 - 4x^2 +14x -22
     double f1(const double &x) {
         return x * x * x - 4 * x * x + 14 * x - 22;
